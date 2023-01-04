9298db78a62888b6b8b1ceeb1f77de4ae952255e
Carlos Fernández Allén <carlos@carferallen.com>
2023-01-04 21:33:44 +0100

    parámetro para autocompletar

--- js/main.js
+++ js/main.js
@@ -159,5 +159,22 @@ const autocompletar = function(){
 
 $(document).ready(function() {
     inicializa();
-    autocompletar();
+    if (GetURLParameter('a')==1) {
+        autocompletar();
+    }
 });
+
+
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
